a29533579dccd65d603931a1f1efa5b6419a5d86
ynskmt <yrtiktwyr.0427@gmail.com>
2020-03-14 13:54:01 +0900

    update:routes
    destroyを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'mypages#index'
   resources :users, only: [:index, :edit, :update]
-  resources :students, only: [:new, :create, :edit, :update] do
+  resources :students, only: [:new, :create, :edit, :update, :destroy] do
     resources :reports, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end

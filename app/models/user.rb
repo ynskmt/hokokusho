11b79bb45505a90b6fc2dca112fbd0c795f1d005
ynskmt <yrtiktwyr.0427@gmail.com>
2020-03-14 21:52:55 +0900

    update:userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_many :reports
-has_many :student_users
+has_many :reports, dependent: :destroy
+has_many :student_users, dependent: :destroy
 has_many :students, through: :student_users
-has_many :comments
+has_many :comments, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :user_status

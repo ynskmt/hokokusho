72de30fbb5281f063af6db78344996d1e77e7a06
ynskmt <yrtiktwyr.0427@gmail.com>
2020-03-27 16:37:40 +0900

    Update report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,6 @@ class Report < ApplicationRecord
   belongs_to :student
   belongs_to :user
   has_many :comments
+
+  validates :year, :month, :day, :unit, :homework_rate, :content, :teacher, :confirmation, :homework, :next_class, presence: true
 end

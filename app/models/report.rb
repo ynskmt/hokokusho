1ad4198a7d640d4e652e020c793a7622d9901fdb
ynskmt <yrtiktwyr.0427@gmail.com>
2020-03-28 22:35:03 +0900

    update:report.rb
    バリデーションを修正
    length maximumを設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,5 +3,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :year, :month, :day, :unit, :homework_rate, :content, :teacher, :confirmation, :homework, :next_class, presence: true
+  validates :year, :month, :day, :homework_rate, :teacher, :confirmation, :next_class, presence: true
+  validates :unit ,length: { maximum: 30}
+  validates :content ,length: { maximum: 300}
+  validates :homework ,length: { maximum: 150}
+
 end

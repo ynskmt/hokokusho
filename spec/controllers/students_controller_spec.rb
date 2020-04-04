5a3b309d1005933bcbef70afc6bedc78dd20dddd
ynskmt <yrtiktwyr.0427@gmail.com>
2020-04-04 17:53:24 +0900

    Create students_controller_spec.rb

---
+++ spec/controllers/students_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe StudentsController, type: :controller do
+
+  # describe 'GET #new' do
+  #   it "renders the :new template" do
+  #     get :new
+  #     expect(response).to render_template :new
+  #   end
+  # end
+end

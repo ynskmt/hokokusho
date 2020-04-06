9460e7183fcf86ebcbb85b0f73c5a28b58dfff23
ynskmt <yrtiktwyr.0427@gmail.com>
2020-04-07 00:42:01 +0900

    Update report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,7 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :year, :month, :day, :homework_rate, :teacher, :confirmation, :next_class, presence: true
+  validates :year, :month, :day, :homework_rate, :teacher, :next_class, presence: true
   validates :unit ,length: { maximum: 30}, presence: true
   validates :content ,length: { maximum: 300}, presence: true
   validates :homework ,length: { maximum: 150}, presence: true

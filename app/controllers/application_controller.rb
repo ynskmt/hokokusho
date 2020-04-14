0ee9f3673667ebaac581aa925e374ab4f0b2c96b
ynskmt <yrtiktwyr.0427@gmail.com>
2020-04-14 23:59:28 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  http_basic_authenticate_with :name => ENV['USER'], :password => ENV['PASS'] if Rails.env == "production"
+
   protected
 
   def configure_permitted_parameters
